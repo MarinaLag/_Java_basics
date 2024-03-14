c4903556de01fc05411d19de50719b93a8f217e4
Marina Lagoikina <marinalagoikina@gmail.com>
2024-03-14 15:10:11 +0300

    fest commit

--- src/main/java/by/_tests/Test_1.java
+++ src/main/java/by/_tests/Test_1.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 public class Test_1 {
     public static void main(String[] args) {
         System.out.println("1 ");
-        synchronized (args){
-            System.out.println("2 ");
-            try {
-                args.wait();
-            }catch (InterruptedException exception){
-                exception.printStackTrace();
-            }
-        }
+//        synchronized (args){
+//            System.out.println("2 ");
+//            try {
+//                args.wait();
+//            }catch (InterruptedException exception){
+//                exception.printStackTrace();
+//            }
+//        }
         System.out.println("3 ");
 
 
