76258d5724a93107496ba07fdea249d7cd76fb1a
Marina Lagoikina <marinalagoikina@gmail.com>
2024-03-19 18:03:09 +0300

    fest commit

--- src/main/java/by/_java_EE/_1_collections/Main.java
+++ src/main/java/by/_java_EE/_1_collections/Main.java
@@ -1,5 +1,7 @@
 package by._java_EE._1_collections;
 
+import by._java_SE._7_class._1_blockCoda.Person;
+
 import java.util.*;
 
 public class Main {
@@ -33,8 +35,45 @@ public class Main {
         Set<Integer> set = new TreeSet<>();
 
         System.out.println("-----------------------");
-        PriorityQueue <Integer> priorityQueue= new PriorityQueue<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
         Queue<Integer> queue1 = new PriorityQueue<>();
+
+
+        System.out.println("------------------");
+        Hashtable<Integer, String> map = new Hashtable<>();
+        map.put(1, "Class");
+        System.out.println(map);    // {1=Class}
+        var value = map.put(1, "Web");
+        System.out.println(value);   //Class
+        System.out.println(map);     // {1=Web}
+
+        System.out.println("------------------");
+        Hashtable<Integer, String> map1 = new Hashtable<>();
+        map1.put(1, "Class");
+        System.out.println(map1);    // {1=Class}
+        var value1 = map1.putIfAbsent(1, "Web");
+        System.out.println(value1);  //Class
+        System.out.println(map1);    //{1=Class}
+
+        System.out.println("------------------");
+        Hashtable<Integer, List<Person>> map2 = new Hashtable<>();
+
+        System.out.println("---------------");
+        map1.put(2, "Java");
+        map1.put(3, "C++");
+        Set<Map.Entry<Integer, String>> entrySet = map1.entrySet();
+        System.out.println(entrySet);  //[3=C++, 2=Java, 1=Class]
+
+        for (Map.Entry<Integer, String> elem : entrySet) {
+            Integer key = elem.getKey();
+            String value3 = elem.getValue();
+            System.out.println("key " + key + "value " + value3);
+        }
+        // key 3value C++
+        //key 2value Java
+        //key 1value Class
+
+
 
 
     }
